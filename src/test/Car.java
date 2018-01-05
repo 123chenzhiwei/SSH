53dcdf26f0bcabe24ff8df9ee89139b08d39b07c
chenzhiwei <1585322645@qq.com>
2018-01-05 16:10:40 +0800

    大哥威

--- src/test/Car.java
+++ src/test/Car.java
@@ -2,6 +2,7 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("ch");
         System.out.println("兰博基尼");
+        System.out.println("法拉利");
 
     }
 }
